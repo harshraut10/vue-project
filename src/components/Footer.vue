05ad3be1ddd37adbfb8057f4efdd4be02025bf8e
harsh_raut <harshraut73@gmail.com>
2023-08-09 16:10:23 +0530

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <footer>
+        <p>Copyrigt &copy; 2023</p>
+        <a to="/about">About</a>
+    </footer>
+</template>
+<style scoped>
+    a{
+        color: #333;
+    }
+
+    footer{
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+    <h3>About</h3>
+    <router-link to="/">go back</router-link>
+</template>
